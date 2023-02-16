176059616ee12eaec58537cd305f8dd6d8a46d18
Nico385412 <nico.hansse@gmail.com>
2023-02-16 22:46:38 +0100

    chore(04): fix issue with lab

--- steps/apps/04-classic-provider/src/app.module.ts
+++ steps/apps/04-classic-provider/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppService } from './app-service/app-service';
 import { AppController } from './app.controller';
 
 @Module({
   imports: [],
-  providers: [AppService],
+  providers: [],
   controllers: [AppController],
 })
 export class AppModule { }
