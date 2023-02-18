2dff0bd84492f9ba7c324cf87b6eb750253c7339
Axel Latour <axel.latour@ext.adeo.com>
2023-02-18 16:45:35 +0100

    fix: remove use of dynamic providers and keep using same todo controller / service

--- steps/apps/06-configuration-env-file-solution/src/feature/todos/providers/todo.service.ts
+++ steps/apps/06-configuration-env-file-solution/src/feature/todos/providers/todo.service.ts
@@ -1,33 +1,19 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
+import { TODOS_LIST } from '../constants/todos-list';
 import { Todo } from '../models/todo.model';
-import { TODOS_MOCKS } from './todos-mocks.service';
 
 @Injectable()
 export class TodoService {
-  constructor(@Inject(TODOS_MOCKS) private readonly todoList: Array<Todo>) {
-  }
-
   getAllTodos(): Array<Todo> {
-    return this.todoList;
+    return TODOS_LIST;
   }
 
-  createTodo(todo: Omit<Todo, 'id'>): number {
-    const idTodo = this.todoList.length + 1;
-    this.todoList.push({ ...todo, id: idTodo });
-    return idTodo;
+  getTodo(idTodo: number): Todo {
+    return TODOS_LIST.find(({ id }) => id === idTodo);
   }
 
-  getTodo(idTodo: number): Todo | Error {
-    const todo: Todo = this.todoList.find(({ id }) => id === idTodo);
-    if (todo) {
-      return todo;
-    }
-    throw new Error(`Ressource not found for id ${idTodo}`);
-  }
-
-  deleteTodo(idTodo: number): void | Error {
-    const indexTodo: number = this.todoList.findIndex(({ id }) => id === idTodo);
-    if (indexTodo === -1) throw new Error(`Ressource not found for id ${idTodo}`);
-    this.todoList.splice(indexTodo, 1);
+  deleteTodo(idTodo: number): void {
+    const indexTodo: number = TODOS_LIST.findIndex(({ id }) => id === idTodo);
+    TODOS_LIST.splice(indexTodo, 1);
   }
 }
