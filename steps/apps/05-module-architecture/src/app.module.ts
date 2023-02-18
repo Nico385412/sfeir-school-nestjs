38ef92ad186585720b9b81c22604b338d6d63bb2
Nico385412 <nico.hansse@gmail.com>
2023-02-18 11:35:54 +0100

    fix(tp5): remove injection not seen yet

--- steps/apps/05-module-architecture/src/app.module.ts
+++ steps/apps/05-module-architecture/src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { TodoService } from './shared/providers/todo.service';
-import { TODOS_MOCKS_PROVIDER } from './shared/providers/todos-mocks.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [TodoService, TODOS_MOCKS_PROVIDER],
+  providers: [TodoService],
 })
 export class AppModule {}
