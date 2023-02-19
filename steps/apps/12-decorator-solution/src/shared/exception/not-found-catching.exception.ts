8ef1af9b8e0ccdb47e94b4692d603b46246a27c2
Axel Latour <axel.latour@ext.adeo.com>
2023-02-19 17:21:42 +0100

    chore(exception-catching): simplify exercise

--- steps/apps/12-decorator-solution/src/shared/exception/not-found-catching.exception.ts
+++ steps/apps/12-decorator-solution/src/shared/exception/not-found-catching.exception.ts
@@ -9,13 +9,10 @@ export class RessourceNotFoundExceptionFilter implements ExceptionFilter {
     const response = context.getResponse<Response>();
     const request = context.getRequest<Request>();
 
-    const [, , endpoint] = request.path.split('/');
-
     return response.status(exception.getStatus()).json({
       ...exception,
       time: new Date(),
       endpoint: request.url,
-      description: `It seems that the ${endpoint} with the id ${request.params.id} doest not existes in our database. Please contact the support`,
     });
   }
 }
