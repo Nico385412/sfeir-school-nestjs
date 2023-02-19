1c06912cf21bb32eb3d81df9cdff24be4aedf1dc
Nicolas Hansse <nico.hansse@gmail.com>
2023-02-19 17:00:40 +0100

    chore(unittests): remove them (#6)

--- steps/apps/10-guards/src/feature/todos/providers/todos-mocks.service.ts
+++
@@ -1,9 +0,0 @@
-import { Provider } from '@nestjs/common';
-import { TODOS_LIST } from '../constants/todos-list';
-
-export const TODOS_MOCKS = 'TODOS';
-
-export const TODOS_MOCKS_PROVIDER: Provider = {
-  provide: TODOS_MOCKS,
-  useValue: TODOS_LIST,
-};
