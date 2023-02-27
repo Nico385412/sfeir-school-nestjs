53c74f52e5ecf376556e41f1973f1e9d19689465
Beatriz Rochelle <rochelle.beatriz@gmail.com>
2023-02-27 11:54:18 +0100

    feat: 03-controller
    add TodoController

--- steps/apps/03-controller/src/app.module.ts
+++ steps/apps/03-controller/src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
+import { TodoController } from 'apps/05-module-architecture-solution/src/feature/todos/todo.controller';
 
 @Module({
   imports: [],
+  controllers: [TodoController]
 })
 export class AppModule {}
